f4553034652cf7d9f5130621f1e8de57e14a4210
Dokanii <tenma.ldpc@gmail.com>
2024-10-30 21:50:17 +0900

    side navbar + css navbar

---
+++ app.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    const content = document.querySelector(".content");
+    sidebar.classList.toggle("collapsed");
+    content.classList.toggle("collapsed");
+}
